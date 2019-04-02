8646ebaa204e048c92bdbc7ce5be27a54b181572
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2019-04-02 23:04:12 +0200

    Zarys kompresora

---
+++ Alchemy-PaletteSampler/Compression.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace Alchemy_PaletteSampler
+{
+    class Compression
+    {
+        private ImageCodecInfo GetEncoder(ImageFormat format)
+        {
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageDecoders();
+            foreach(ImageCodecInfo codec in codecs)
+            {
+                if(codec.FormatID == format.Guid)
+                {
+                    return codec;
+                }
+            }
+            return null;
+        }
+
+        public Compression(Bitmap inputMap)
+        {
+            using (inputMap)
+            {
+
+            }
+        }
+    }
+}
