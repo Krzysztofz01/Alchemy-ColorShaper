2fd364730fc4cf92aee8b8504e47b6f1c305686a
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2023-04-25 18:47:45 +0200

    Fea: Importer and Exporter implemented

---
+++ src/AlchemyColorShaper/AlchemyColorShaper.Core/Exporter.cs
@@ -0,0 +1,33 @@
+﻿using SkiaSharp;
+using System.IO;
+
+namespace AlchemyColorShaper.Core;
+
+public static class Exporter
+{
+    public static void ExportPalleteImageAsJpegFile(string filePath, Palette palette, int height, bool inverted)
+    {
+        var image = palette.GeneratePaletteImage(height, inverted);
+
+        using var memoryStream = new MemoryStream();
+        using var managedWStream = new SKManagedWStream(memoryStream);
+        image.Encode(managedWStream, SKEncodedImageFormat.Jpeg, 100);
+
+        using var fileStream = new FileStream(filePath, FileMode.Create);
+        memoryStream.Seek(0, SeekOrigin.Begin);
+        memoryStream.CopyTo(fileStream);
+    }
+
+    public static void ExportPalleteImageAsPngFile(string filePath, Palette palette, int height, bool inverted)
+    {
+        var image = palette.GeneratePaletteImage(height, inverted);
+
+        using var memoryStream = new MemoryStream();
+        using var managedWStream = new SKManagedWStream(memoryStream);
+        image.Encode(managedWStream, SKEncodedImageFormat.Png, 100);
+
+        using var fileStream = new FileStream(filePath, FileMode.Create);
+        memoryStream.Seek(0, SeekOrigin.Begin);
+        memoryStream.CopyTo(fileStream);
+    }
+}
---
+++ src/AlchemyColorShaper/AlchemyColorShaper.Core/Importer.cs
@@ -0,0 +1,15 @@
+﻿using SkiaSharp;
+using System.IO;
+
+namespace AlchemyColorShaper.Core;
+
+public static class Importer
+{
+    public static SKBitmap ImportImage(string filePath)
+    {
+        using var fileStream = File.Open(filePath, FileMode.Open);
+        var image = SKBitmap.Decode(fileStream);
+
+        return image;
+    }
+}
