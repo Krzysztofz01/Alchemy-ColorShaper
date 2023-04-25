e6b8db8d86ee03ef73ccde1a106ab581f51bc9e0
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2023-04-25 18:50:34 +0200

    Fea: Added a simple testing entrypoint

---
+++ src/AlchemyColorShaper/AlchemyColorShaper.Cli/Program.cs
@@ -0,0 +1,37 @@
+﻿using AlchemyColorShaper.Core;
+using System;
+
+namespace AlchemyColorShaper.Cli
+{
+    internal class Program
+    {
+        private const double _scaleFactor = 0.4;
+
+        // TODO: Implement flags and clean up the entrypoint
+        static void Main(string[] args)
+        {
+            if (args.Length < 1) Environment.Exit(1);
+            var image = Importer.ImportImage(args[0]);
+
+            var paletteCountingMethod = PaletteGenerator.GeneratePalette(image, new PaletteGeneratorOptions
+            {
+                Algorithm = PaletteGeneratorAlgorithm.CountingMethod,
+                ScaleFactor = _scaleFactor,
+                ColorThreshold = 250,
+                MaxEntries = 7
+            });
+
+            var paletteTensorMethod = PaletteGenerator.GeneratePalette(image, new PaletteGeneratorOptions
+            {
+                Algorithm = PaletteGeneratorAlgorithm.TensorMethod,
+                ScaleFactor = _scaleFactor,
+                MaxEntries = 7
+            });
+
+            Exporter.ExportPalleteImageAsPngFile("palette-counting-method.png", paletteCountingMethod, 800, false);
+            Exporter.ExportPalleteImageAsPngFile("palette-tensor-method.png", paletteTensorMethod, 800, false);
+
+            Environment.Exit(0);
+        }
+    }
+}
